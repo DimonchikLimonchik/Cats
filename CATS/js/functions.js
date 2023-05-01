cfa1262d447d6de2f634d909a9dc9c35b0c74ea3
DimonchikLimonchik <102550250+DimonchikLimonchik@users.noreply.github.com>
2023-05-01 19:47:04 +0300

    Add files via upload

---
+++ CATS/js/functions.js
@@ -0,0 +1,59 @@
+/*
+    Полиморфный код - переиспользование кода (функции, классы)
+    Инкапсуляция - код не должен выполнять то, что от него не 
+    требуется
+    sum(a, b) {
+        //console.log(a+b) - в консоль НЕ надо выводить!
+        return a + b;
+    }
+    sum(2, 3)
+*/
+function createCard(pet, tag) {
+    const card = document.createElement("div");
+    card.className = "card";
+    const cardImg = document.createElement("div");
+    cardImg.className = "pic";
+    if (pet.image) {
+        cardImg.style.backgroundImage = `url(${pet.image})`;
+    }else{
+        cardImg.classList.add("tmp");
+    }
+    const cardTitle = document.createElement("h2");
+    cardTitle.innerText = pet.name;
+
+    const cardLike = document.createElement("i");
+    cardLike.className = "like fa-heart";
+    cardLike.classList.add(pet.favourite ? "fa-solid" :
+    "fa-regular");
+    cardLike.addEventListener("click", e => {
+        setLike(cardLike, pet.id, !pet.favourite)
+    });
+    card.append(cardImg, cardTitle, cardLike);
+    tag.append(card);
+    //cardImg.style.height = cardImg.offsetWidth + "px";
+}
+
+function setLike(el, id, like) {
+    el.classList.toggle("fa-solid");
+    el.classList.toggle("fa-regular");
+
+    fetch(path + "/update/" + id, {
+        method: "put",
+        // без headers на сервер прийдет undefined
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({favorite: like})
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        pets = pets.map(p => {
+            if (p.id === id) {
+                p.favourite = like;
+            }
+            return p;
+        })
+        localStorage.setItem("band-cats", JSON.stringify(pets));
+    })
+}
---
+++ CATS/js/global.js
@@ -0,0 +1,27 @@
+const block = document.querySelector(".wrapper");
+const addBtn = document.querySelector(".add");
+const mdBox = document.querySelector(".modal-block");
+const mdClose = mdBox.querySelector(".modal-close");
+
+const addForm = document.forms.add;
+const prevTag = addForm.querySelector(".preview");
+
+
+let name = "DimonchikLimonchik";
+let path = `https://cats.petiteweb.dev/api/
+single/${"DimonchikLimonchik"}`;
+
+let pets = localStorage.getItem("band-cats");
+// let pets = localStorage.getItem("unic");
+if (pets) {
+    try {
+        pets = JSON.parse(pets);
+        for (let pet of pets) {
+            createCard(pet, block);
+        }
+    } catch(err) {
+        console.warn(err.message);
+        pets = null;
+    }
+}
+console.log("pets", pets);
